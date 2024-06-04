d05be0307d8e1e84f57b927b2a29ae96fd1f0dc3
Judah Almanzor <154279144+BrwnStallion@users.noreply.github.com>
2024-06-04 12:25:50 -0700

    docs: add preliminary pseudocode for main objects

--- script.js
+++ script.js
@@ -2,7 +2,28 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Pseudocode ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+/* 
+Players
+- Each player can have an object that stores relevant information, actions
+    - Properties: Name, win-loss record, player number?
+    - Methods: Make a move
+- Players can be stored in a larger player object
 
+Game Flow
+- Game Rules
+    - Play alternates between player1 and player2
+    - Each player has a unique selector icon (X and O)
+    - A player must only select unselected cells
+- Game End Condition:
+    - Win or Draw
+    - Win Condition:
+        - A player has 3 selectors in at least one row/column/diagonal
+    - Draw Condition:
+        - The gameboard is filled with no win condition
+
+Gameboard
+- Need some object to store the locations/decisions in the gameboard grid
+*/
 
 
 'use strict';
